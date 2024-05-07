6063920c586411f2d6faf6936545a9477ad6a343
shaquille <69906378+koplomen69@users.noreply.github.com>
2024-05-07 23:01:09 +0700

    Add files via upload

---
+++ storage/framework/views/a9d49d03132218c3786a97c3db2fe13c.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>
+
+<?php /**PATH E:\SI pemrograman framework\shaquille_authentication\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/b54c02d5d9ff497797f2f9ddaf9c3719.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>: <?php echo e($url); ?>
+
+<?php /**PATH E:\SI pemrograman framework\shaquille_authentication\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/button.blade.php ENDPATH**/ ?>
